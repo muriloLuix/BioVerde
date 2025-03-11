8761064fed8e74dec26be8803ecc9e87723a36d8
Fernando Kotinda <fernando.kb077@gmail.com>
2025-03-11 13:03:54 -0300

    Instalação do Axios para conexão do front-end com o back-end

--- front-end/src/components/LoginForm.tsx
+++ front-end/src/components/LoginForm.tsx
@@ -1,21 +1,41 @@
+import { useState } from "react";
+import axios from "axios";
 
 export default function LoginForm() {
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+        const response = await axios.post("http://localhost/bioverde-backend/api/login.php", {
+            user,
+            email,
+            password
+        });
+        alert(response.data.message);
+    } catch  {
+        alert("Erro ao conectar com o servidor.");
+    }
+  };
+
     return (
-        <form className="flex flex-col gap-6">
+        <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
 
           <div className="flex flex-col gap-1">
             <label htmlFor="user" className="font-[kufam] text-lg shadow-text">Usuário:</label>
-            <input type="text" id="user" placeholder="Usuário" className="p-2 rounded text-black bg-brancoSal" />
+            <input type="text" id="user" placeholder="Usuário" value={user} onChange={(e) => setUser(e.target.value)} className="p-2 rounded text-black bg-brancoSal" />
           </div>
 
           <div className="flex flex-col gap-1">
             <label htmlFor="email" className="font-[kufam] text-lg shadow-text">Email:</label>
-            <input type="email" id="email" placeholder="E-mail" className="p-2 rounded text-black bg-brancoSal" />
+            <input type="email" id="email" placeholder="E-mail" value={email}  onChange={(e) => setEmail(e.target.value)} className="p-2 rounded text-black bg-brancoSal" />
           </div>
 
           <div className="flex flex-col gap-1">
             <label htmlFor="password" className="font-[kufam] text-lg shadow-text">Senha:</label>
-            <input type="password" id="password" placeholder="Senha" className="p-2 rounded text-black bg-brancoSal" />
+            <input type="password" id="password" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)} className="p-2 rounded text-black bg-brancoSal" />
           </div>
 
           <div className="flex justify-between items-center mt-1">
@@ -26,7 +46,7 @@ export default function LoginForm() {
             <a href="#" className="font-[kufam] text-sm text-gray-300 hover:underline italic">Esqueceu a senha?</a>
           </div>
           
-          <button className="bg-verdePigmento cursor-pointer tracking-wide w-[200px] h-12 m-auto p-2 rounded text-white font-[koulen] hover:bg-verdeGrama transition text-2xl sombra">ENTRAR</button>
+          <button type="submit" className="bg-verdePigmento cursor-pointer tracking-wide w-[200px] h-12 m-auto p-2 rounded text-white font-[koulen] hover:bg-verdeGrama transition text-2xl sombra">ENTRAR</button>
         </form>
     );
 }
