888d2b51db080294de17ffb246e6eefa21a8c656
muriloLuix <muriloluiz654@gmail.com>
2025-03-20 12:58:49 -0300

    ALterando para banco de dados remoto

--- back-end/ambiente.inc.php
+++ back-end/ambiente.inc.php
@@ -1,16 +1,21 @@
 <?php
-// Configuração do banco de dados
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '123456');
-define('DB_NAME', 'bioverde');
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-// Criar conexão com MySQL
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+// Configuração do banco de dados remoto
+define('DB_HOST', '162.241.203.227'); // IP do servidor
+define('DB_USER', 'goecho42_murilo_echo'); // Usuário do banco
+define('DB_PASS', 'Murylindos1204*'); // Senha do banco
+define('DB_NAME', 'goecho42_bioverde'); // Nome do banco
+define('DB_PORT', 3306); // Porta padrão do MySQL
+
+// Criar conexão com MySQLi
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 
 // Verificar conexão
 if ($conn->connect_error) {
-    die(json_encode(["success" => false, "message" => "Erro na conexão com o banco de dados"]));
+    die(json_encode(["success" => false, "message" => "Erro na conexão com o banco de dados: " . $conn->connect_error]));
+} else {
+    // echo "Conectou com sucesso!";
 }
-
 ?>
--- back-end/login/login.php
+++ back-end/login/login.php
@@ -7,6 +7,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+
 // Verificação se a requisição é do tipo OPTIONS (preflight) e responde com sucesso
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
