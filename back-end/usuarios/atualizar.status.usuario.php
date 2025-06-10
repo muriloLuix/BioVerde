906ef3cf3e3e65d1741ca6d0f8e237d82721b3de
muriloLuix <muriloluiz654@gmail.com>
2025-06-10 18:24:29 -0300

    Refatoração de código

--- back-end/usuarios/atualizar.status.usuario.php
+++ back-end/usuarios/atualizar.status.usuario.php
@@ -1,30 +1,31 @@
 <?php
-ini_set("display_errors", 1);
+/**************** HEADERS ************************/
 session_start();
 include_once "../inc/funcoes.inc.php";
 header_remove('X-Powered-By');
 header('Content-Type: application/json');
+verificarAutenticacao($conn, $_SESSION['user_id']);
+/*************************************************/
 
 try {
-    // Verifica autenticação
-    if (!isset($_SESSION["user_id"])) {
-        checkLoggedUser($conn, $_SESSION['user_id']);
-        exit;
-    }
-
-    // Recebe JSON do front
+    /**************** RECEBE AS INFORMAÇÕES DO FRONT-END ************************/
     $rawData = file_get_contents("php://input");
     $data = json_decode($rawData, true);
     if (!$data || !isset($data['user_id'], $data['estaAtivo'])) {
         throw new Exception("Dados inválidos.");
     }
 
-    $userId = (int)$data['user_id'];
-    $novoStatus = (int)$data['estaAtivo'];
+    $userId = (int) $data['user_id'];
+    $novoStatus = (int) $data['estaAtivo'];
+    /**************************************************************************/
+
+    /**************** VERIFICA SE O USUÁRIO EXISTE ************************/
 
     if (!verifyExist($conn, $userId, "user_id", "usuarios")) {
         throw new Exception("Usuário não encontrado.");
     }
+
+    /**********************************************************************/
 
     $stmt = $conn->prepare("UPDATE usuarios SET estaAtivo = ?, force_logout = 1 WHERE user_id = ?");
     $stmt->bind_param("ii", $novoStatus, $userId);
@@ -41,5 +42,6 @@ try {
         "message" => $e->getMessage()
     ]);
 } finally {
-    if (isset($conn)) $conn->close();
+    if (isset($conn))
+        $conn->close();
 }
